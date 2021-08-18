f155d7c5fb02b3a35a9638f724139612aab07aa7
unknown <fabriciosouza21777@gmail.com>
2021-08-18 16:15:49 -0300

    utilizando css condicional

---
+++ exercicios/components/EstiloCondicional.jsx
@@ -0,0 +1,8 @@
+export default function Estilo(props){
+    return (
+            <h1 style = {{backgroundColor: props.number >=0 ? "#2D2" : "#D22"}}>  
+            texto
+            </h1>
+
+    )
+}
---
+++ exercicios/pages/css/usandoEstilo.jsx
@@ -0,0 +1,11 @@
+import Estilo from "../../components/estilo"
+
+export default function usandoEstilo(){
+    return (
+        <div>
+            <Estilo number = {3}/>
+            <Estilo number = {-1}/>
+
+        </div>
+    )
+}
