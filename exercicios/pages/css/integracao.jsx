6e07b7c4dfb50749e5f79042cab3547455a20253
unknown <fabriciosouza21777@gmail.com>
2021-08-18 15:26:53 -0300

    integrando os componentes com css

---
+++ exercicios/pages/css/integracao.jsx
@@ -0,0 +1,12 @@
+import style from "./integracao.module.css"
+
+export default function integracao(){
+    return (
+        <div id= {style.integracao}>
+        <div className = {style.azul}> Azul </div>
+        <div className = {style.vermelho}> vermelho  </div>
+        <div className = {style.verde}> verde </div>
+        </div>
+    
+    )
+}
